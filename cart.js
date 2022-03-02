4526ee02a2168d10118d82c21fd020d0fc790863
Will <wei770708@gmail.com>
2022-03-03 01:30:53 +0800

    w5

--- cart.js
+++ cart.js
@@ -30,36 +30,101 @@ const app = Vue.createApp({
         return {
             cartData: {},
             products: [],
+            productId: '',
+            isLoadingItem: '',
         };
     },
     methods: {
         getProducts() {
-            axios.get(`${apiUrl}/api​/${apiPath}​/products​/all`)
+            axios.get(`${apiUrl}/api/${apiPath}/products/all`)
             .then((res) => {
             this.products = res.data.products;
             });
         },
-        openProductModal() {
+        openProductModal(id) {
+            this.productId = id;
             this.$refs.productModal.openModal();
+        },
+        getCart() {
+            axios.get(`${apiUrl}/api/${apiPath}/cart`)
+            .then((res) => {
+            this.cartData = res.data.data;
+            });
+        },
+        addToCart(id, qty = 1) {
+            const data = {
+                product_id: id,
+                qty,
+            };
+            this.isLoadingItem = id;
+            axios.post(`${apiUrl}/api/${apiPath}/cart`, { data })
+            .then((res) => {
+                this.getProduct();
+                this.$refs.productModal.closeModal();
+                this.isLoadingItem = '';
+            });
+        },
+        remobeCartItem(id) {
+            this.isLoadingItem = id;
+            axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`)
+            .then((res) => {
+                this.getCart();
+                this.isLoadingItem = '';
+            });
+        },
+        updateCartItem(item) {
+            const data = {
+                product_id: item.id,
+                qty: item.qty,
+            };
+            this.isLoadingItem = item.id;
+            axios.put(`${apiUrl}/api/${apiPath}/cart/${item.id}`, { data })
+            .then((res) => {
+                this.getProduct();
+                this.isLoadingItem = '';
+            });
         },
     },
     mounted() {
         this.getProducts();
+        this.getCart();
     },
 });
 
 app.component('product-modal', {
+    props: ['id'],
     template: '#userProductModal',
-    fata() {
-
+    data() {
+        return {
+modal: {},
+product: {},
+qty: 1,
+        };
+    },
+    watch: {
+id() {
+this.getProduct();
+},
     },
     methods: {
         openModal() {
-
-        }
+this.modal.show();
+        },
+        closeModal() {
+            this.modal.hide();
+        },
+        getProduct() {
+            axios.get(`${apiUrl}/api/${apiPath}/product/${this.id}`)
+            .then((res) => {
+            this.product = res.data.product;
+            });
+        },
+        addToCart() {
+            this.$emit('add-cart', this.product.id, this.qty);
+        },
     },
     mounted() {
-        const myModal = new bootstrap.Modal(this.$refs.modal);
+        this.modal = new bootstrap.Modal(this.$refs.modal);
     },
 });
 
